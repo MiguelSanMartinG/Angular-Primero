d61bb63a74c648169d295f8c2b98b3532d6205b2
Miguel <miguel.sanmartin@susess.com>
2023-01-28 10:40:23 -0600

    commit

---
+++ src/app/Tierra/personajes/personajes.component.ts
@@ -0,0 +1,16 @@
+import { Component, Input, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-personajes',
+  templateUrl: './personajes.component.html',
+  styleUrls: ['./personajes.component.css']
+})
+export class PersonajesComponent implements OnInit {
+
+  @Input() personajes : any[] = [];
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
